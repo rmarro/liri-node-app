b7bede22a17db3c9ea13817fbddc2af3ac4580fb
rmarro <rmarro47@gmail.com>
2018-02-01 15:13:43 -0600

    created getTweets function, currently linked and retrieves one tweet

--- liri.js
+++ liri.js
@@ -1,28 +1,40 @@
+// Set up process.env so keys can be accessed
 require("dotenv").config();
-
 var keys = require("./keys.js");
 
-var spotify = keys.spotify;
-var client = keys.twitter;
-
+// Save user input as the command and optional request term
 var userCommand = process.argv[2];
 var userRequest = process.argv[3];
 
+// Set up Twitter client
+var Twitter = require('twitter');
+var client = new Twitter(keys.twitter);
+
+// Set up Spotify 
+// var spotify = keys.spotify;
+
+// Possible command cases
 switch(userCommand) {
     case "my-tweets":
-       console.log("tweets will show here");
-
-        break;
+        getTweets();
+    break;
     case "spotify-this-song":
-
-
-        break;
+    
+    break;
     case "movie-this":
-       
-
-        break;
+    
+    break;
     case "do-what-it-says":
     
-
     break;
 };
+
+
+// Get tweets (TO DO: add count limit and log all of array not just tweets[0])
+function getTweets() {
+    client.get('statuses/user_timeline', {screen_name: "liri_app"}, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets[0].created_at + ": " + tweets[0].text)
+        }
+    });
+}
