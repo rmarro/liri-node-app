953b6ce1b859339bcae89e0893d5fc23444ce2cd
rmarro <rmarro47@gmail.com>
2018-02-03 22:16:59 -0600

    made function to get data from random.txt

--- liri.js
+++ liri.js
@@ -17,36 +17,44 @@ var clientSpotify = new Spotify(keys.spotify);
 // Import the request package for omdb
 var request = require("request");
 
+// Import fs package for reading and writing files
+var fs = require("fs");
+
 // Possible command cases
-switch(userCommand) {
-    case "my-tweets":
-        getTweets();
-    break;
-    case "spotify-this-song":
-        if (userRequest === "") {
-            userRequest = "The Sign Ace of Base";
-        };
-        getSong();
-    break;
-    case "movie-this":
-        if (userRequest === "") {
-            userRequest = "Mr Nobody";
-        };
-        getMovie();
-    break;
-    case "do-what-it-says":
-    
-    break;
+function heyLiri() {
+    switch(userCommand) {
+        case "my-tweets":
+            getTweets();
+        break;
+        case "spotify-this-song":
+            if (userRequest === "") {
+                userRequest = "The Sign Ace of Base";
+            };
+            getSong();
+        break;
+        case "movie-this":
+            if (userRequest === "") {
+                userRequest = "Mr Nobody";
+            };
+            getMovie();
+        break;
+        case "do-what-it-says":
+            getRandom();
+        break;
+    };
 };
+
+heyLiri();
 
 
 // Get 20 most recent tweets and console.log timestamp with text
 function getTweets() {
     clientTwitter.get('statuses/user_timeline', {screen_name: "liri_app", count: 20}, function (error, tweets, response) {
-        if (!error) {
-            for (i = 0; i < tweets.length; i++) {
-                console.log(tweets[i].created_at + ": " + tweets[i].text);
-            };
+        if (error) {
+            return console.log("Error: " + error);
+        };
+        for (i = 0; i < tweets.length; i++) {
+            console.log(tweets[i].created_at + ": " + tweets[i].text);
         };
     });
 };
@@ -54,29 +62,46 @@ function getTweets() {
 // Search by song name and return artist, song, album, and link
 function getSong() {
     clientSpotify.search({type: 'track', query: userRequest, limit: 1}, function(error, data) {
-        if (!error) {
-            var mainInfo = data.tracks.items[0];
-            
-            console.log("Artist: " + mainInfo.album.artists[0].name + "\nSong Name: " + mainInfo.name + "\nAlbum: " + mainInfo.album.name + "\nLink: " + mainInfo.external_urls.spotify);
+        if (error) {
+            return console.log("Error: " + error);
         };
+
+        var mainInfo = data.tracks.items[0];
+        
+        console.log("Artist: " + mainInfo.album.artists[0].name + "\nSong Name: " + mainInfo.name + "\nAlbum: " + mainInfo.album.name + "\nLink: " + mainInfo.external_urls.spotify);  
     });     
 };
 
 // Search by movie name and return title, year, imdb rating, RT rating, country, language, plot, and actors
 function getMovie() {
     request("http://www.omdbapi.com/?t=" + userRequest +"&y=&plot=short&apikey=trilogy", function (error, response, body) {
+        if (error) {
+            return console.log("Error: " + error);
+        };
         if (!error && response.statusCode === 200) {
+
             var mainInfo = JSON.parse(body);
+
             console.log("Title: " + mainInfo.Title + "\nYear: " + mainInfo.Year + "\nIMDB Rating: " + mainInfo.imdbRating + "\nRotten Tomatoes Rating: " + mainInfo.Ratings[1].Value + "\nCountry Produced: " + mainInfo.Country + "\nLanguage: " + mainInfo.Language + "\nPlot: " + mainInfo.Plot + "\nActors: " + mainInfo.Actors);
         }
     });
 };
 
+// Read the random text file and use that as the command and request
+function getRandom() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        };
 
-// do what it says
-// somehow import text from random.txt
-// splice or slice or something at comma
-// use before as command, after as request
+        var dataArr = data.split(",");
+        userCommand = dataArr[0];
+        userRequest = dataArr[1];
+
+        heyLiri();
+    });
+}
+
 
 
 //bonus
